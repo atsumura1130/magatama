793adf73f45a8d25d7f2aea86793249ad76c0071
Akira Tsumura <tsumura@littlebits.co.jp>
2018-04-19 23:55:58 +0900

    fix bugs.

--- magatama_notify.sample.sh
+++ magatama_notify.sample.sh
@@ -3,11 +3,12 @@
 # put '/root/bin/magatama_slack.sh', no +x permission.
 # Change for your slack bot.
 
-DATA=`echo '{"text":"'${MSG}'('${MSG_STATUS')"}'`
+DATA=`echo '{"text":"'${MSG}'('${MSG_STATUS}')"}'`
 curl \
  -X POST \
  -H 'Content-type: application/json' \
  --data $"{DATA}"" \
- https://hooks.slack.com/services/XXXXXXXX/Saya-chan_Daisuki
+ https://hooks.slack.com/services/XXXXXXXX/Saya-chan_Daisuki \
+ > /dev/null
 
 # do not write 'exit'.
