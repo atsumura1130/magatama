6c346eed9aa58fcc463af8aa2a0ef3183903e57c
Akira Tsumura <tsumura@littlebits.co.jp>
2018-04-21 19:22:19 +0900

    `magatama_notify.sample.sh`が動かないバグを修正。

--- magatama_notify.sample.sh
+++ magatama_notify.sample.sh
@@ -1,14 +1,19 @@
 # ---
+# MAGATAMA common script.
 # Slack Notyfy Sample
 # put '/root/bin/magatama_slack.sh', no +x permission.
 # Change for your slack bot.
 
+# Configure
+SLACK_API="https://hooks.slack.com/services/XXXXXXXX/Saya-chan_Daisuki"
 DATA=`echo '{"text":"'${MSG}'('${MSG_STATUS}')"}'`
+
+# Post comment.
 curl \
  -X POST \
  -H 'Content-type: application/json' \
- --data $"{DATA}"" \
- https://hooks.slack.com/services/XXXXXXXX/Saya-chan_Daisuki \
+ --data $"{DATA}" \
+ ${SLACK_API} \
  > /dev/null
 
-# do not write 'exit'.
+# do not 'exit'.
